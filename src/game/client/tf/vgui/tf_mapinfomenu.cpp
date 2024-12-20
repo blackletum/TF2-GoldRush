3a6e17be40fc520a357f51ef80b18db71d9e8e3c
conneath <lucamarin2007@gmail.com>
2024-12-20 14:19:21 +0200

    Hopefully fix garbage characters in map info text
    i copied the code from live and it seems to work, hooray

--- src/game/client/tf/vgui/tf_mapinfomenu.cpp
+++ src/game/client/tf/vgui/tf_mapinfomenu.cpp
@@ -394,40 +394,56 @@ void CTFMapInfoMenu::LoadMapPage( const char *mapName )
 	FileHandle_t f = g_pFullFileSystem->Open( mapRES, "rb" );
 
 	// read into a memory block
-	int fileSize = g_pFullFileSystem->Size(f);
-	int bufSize = fileSize + 2;
-	char *memBlock = new char[bufSize];
-	g_pFullFileSystem->Read(memBlock, fileSize, f);
-	ucs2 *pUCS2 = (ucs2 *)memBlock;
+	int fileSize = g_pFullFileSystem->Size( f );
+	int dataSize = fileSize + sizeof( wchar_t );
+	if ( dataSize % 2 )
+		++dataSize;
+	wchar_t* memBlock = (wchar_t*)malloc( dataSize );
+	memset( memBlock, 0x0, dataSize );
+	int bytesRead = g_pFullFileSystem->Read( memBlock, fileSize, f );
+	if ( bytesRead < fileSize )
+	{
+		// NULL-terminate based on the length read in, since Read() can transform \r\n to \n and
+		// return fewer bytes than we were expecting.
+		char* data = reinterpret_cast<char*>(memBlock);
+		data[bytesRead] = 0;
+		data[bytesRead + 1] = 0;
+	}
 
-	// null-terminate the stream
-	pUCS2[bufSize - 1] = 0;
-
+#ifndef WIN32
+	if ( ((ucs2*)memBlock)[0] == 0xFEFF )
+	{
+		// convert the win32 ucs2 data to wchar_t
+		dataSize *= 2;// need to *2 to account for ucs2 to wchar_t (4byte) growth
+		wchar_t* memBlockConverted = (wchar_t*)malloc( dataSize );
+		V_UCS2ToUnicode( (ucs2*)memBlock, memBlockConverted, dataSize );
+		free( memBlock );
+		memBlock = memBlockConverted;
+	}
+#else
+	// null-terminate the stream (redundant, since we memset & then trimmed the transformed buffer already)
+	memBlock[dataSize / sizeof( wchar_t ) - 1] = 0x0000;
+#endif
 	// check the first character, make sure this a little-endian unicode file
-	if ( LittleShort( pUCS2[0] ) != 0xFEFF )
+	if ( memBlock[0] != 0xFEFF )
 	{
 		// its a ascii char file
-		m_pMapInfo->SetText( memBlock );
+		m_pMapInfo->SetText( reinterpret_cast<char*>(memBlock) );
 	}
 	else
 	{
-		// convert UCS-2 LE buffer to wide string
-		wchar_t *wBuf = new wchar_t[bufSize];
-		V_UCS2ToUnicode( pUCS2, wBuf, bufSize * sizeof( wchar_t ) );
-
 		// ensure little-endian unicode reads correctly on all platforms
 		CByteswap byteSwap;
 		byteSwap.SetTargetBigEndian( false );
-		byteSwap.SwapBufferToTargetEndian( wBuf, wBuf, bufSize );
+		byteSwap.SwapBufferToTargetEndian( memBlock, memBlock, dataSize / sizeof( wchar_t ) );
 
-		m_pMapInfo->SetText( wBuf + 1 );
-		delete[] wBuf;
+		m_pMapInfo->SetText( memBlock + 1 );
 	}
 	// go back to the top of the text buffer
 	m_pMapInfo->GotoTextStart();
 
 	g_pFullFileSystem->Close( f );
-	delete[] memBlock;
+	free( memBlock );
 
 	// we haven't loaded a valid map image for the current map
 	if ( m_pMapImage && !m_pMapImage->IsVisible() )
